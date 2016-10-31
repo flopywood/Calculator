49cf1995a10a837e68e434e6e0bda42dad4aa131
Florencia <flopywood@Florencias-MacBook-Pro-2.local>
2016-10-31 16:59:35 -0300

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+  var onScreen = "";
+
+  $('.c').click(function() {
+    onScreen = "";
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+  $('.dot').click(function() {
+    onScreen += this.value;
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+  $('.num').click(function() {
+    onScreen += this.value;
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+  $('.add').click(function(a, b) {
+    onScreen += this.value;
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+  $('.subtract').click(function() {
+    onScreen += this.value;
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+  $('.multiply').click(function() {
+    onScreen += this.value;
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+  $('.divide').click(function() {
+    onScreen += this.value;
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+  $('.equal').click(function() {
+    onScreen = eval(onScreen);
+    document.getElementById('screen').innerHTML = "<p class='onscreen'>" + onScreen + "</p>";
+  });
+
+});
